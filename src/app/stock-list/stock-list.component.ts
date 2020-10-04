f6d2cd4ed7a090fcea63b0f46b667ca418dc40b8
Mustafa Bereket <mbereke1@binghamton.edu>
2020-10-03 19:01:20 -0700

    updateAsset method added to prevent data flush each time

--- src/app/stock-list/stock-list.component.ts
+++ src/app/stock-list/stock-list.component.ts
@@ -1,13 +1,13 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
-import {MockService} from '../mock/mock.service';
-import {MockConsumerService} from '../mockConsumer/mock-consumer.service';
-import {MatSort} from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MockService } from '../mock/mock.service';
+import { MockConsumerService } from '../mockConsumer/mock-consumer.service';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-stock-list',
   templateUrl: './stock-list.component.html',
-  styleUrls: ['./stock-list.component.scss']
+  styleUrls: ['./stock-list.component.scss'],
 })
 export class StockListComponent implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['id', 'assetName', 'price', 'lastUpdate'];
@@ -24,18 +24,20 @@ export class StockListComponent implements OnInit, AfterViewInit {
   @ViewChild('table1') table1: any;
   @ViewChild('table1') table2: any;
 
-
-  constructor(private mockService: MockService, private data: MockConsumerService) {}
+  constructor(
+    private mockService: MockService,
+    private data: MockConsumerService
+  ) {}
 
   ngOnInit(): void {
-    this.data.getAssets.subscribe(assetList => {
-      this.stocks = assetList.stock;
-      this.currency = assetList.currency;
+    this.data.getAssets.subscribe((assetList) => {
+      this.updateAssets(assetList.stock, 'stocks');
+      this.updateAssets(assetList.currency, 'currency');
       this.dataSourceCurrency.data = Object.values(assetList.currency);
       this.dataSourceStock.data = Object.values(assetList.stock);
     });
 
-    this.data.favoritesSubscription.subscribe(data => {
+    this.data.favoritesSubscription.subscribe((data) => {
       this.favorites = data;
     });
   }
@@ -48,6 +50,17 @@ export class StockListComponent implements OnInit, AfterViewInit {
   addFavorite(id): void {
     if (!this.favorites.includes(id)) {
       this.data.addFavorite(id);
+    }
+  }
+
+  updateAssets(assetList, type) {
+    for (const key in assetList) {
+      if (this[type][key]) {
+        this[type][key].lastUpdate = assetList[key].lastUpdate;
+        this[type][key].price = assetList[key].price;
+      } else {
+        this[type][key] = assetList[key];
+      }
     }
   }
 }
